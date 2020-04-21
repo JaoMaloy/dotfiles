b7381de5e9b00778f9d83a4537303e851ee8292e
jaomaloy <jaomaloy@gmail.com>
2020-04-21 06:31:48 +0000

    Add polybar initial config and .sh file

---
+++ polybar/launch.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Launch topbar and bottombar
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar topbar >> /tmp/polybar1.log 2>&1 &
+polybar bottombar >> /tmp/polybar2.log 2>&1 &
+
+echo "Bars launched..."
