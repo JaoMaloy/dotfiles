b5b4152d366ad4a67ad8c60ca68b9da02d9e7bdc
jaomaloy <jaomaloy@protonmail.com>
2020-06-27 19:52:41 +0800

    [bspwm] Use bspwm as the current WM
    - Two monitor bspwm set-up with 3 workspaces each
    - bspwmrc laucnhes sxhkdrc, comtpon, and polybar
    - polybar-bottombar has a bspwm module & another
    polybar bar is added for the second monitor
    - default sxhkdrc with st as terminal, a new hotkey
    for rofi & firefox

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,9 +4,8 @@
 killall -q polybar
 
 # Launch topbar and bottombar
-echo "---" | tee -a /tmp/polybar2.log
-# /tmp/polybar1.log
-# polybar topbar >> /tmp/polybar1.log 2>&1 &
-polybar bottombar >> /tmp/polybar2.log 2>&1 &
+echo "---" | tee -a /tmp/polybar.log /tmp/polybar_ext.log
+polybar bottombar >> /tmp/polybar.log 2>&1 &
+polybar bottombar_external >> /tmp/polybar_ext.log 2>&1 &
 
 echo "Bars launched..."
