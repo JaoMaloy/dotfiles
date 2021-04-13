f0e5d4d284f2cca7dcbb466ea49f1d25e1d118bb
jaomaloy <jaomaloy@protonmail.com>
2021-04-13 23:41:07 +0800

    setup script for all dotfiles

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+bspwm_dir=$HOME/.config/bspwm
+sxhkd_dir=$HOME/.config/sxhkd
+compton_dir=$HOME/.config/compton
+polybar_dir=$HOME/.config/polybar
+
+[ -d $nvim_dir ] || mkdir $nvim_dir
+[ -d $nvim_lua_dir ] || mkdir $nvim_lua_dir
+[ -d $nvim_themes_dir ] || mkdir $nvim_themes_dir
+
